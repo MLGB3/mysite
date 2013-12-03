a8312fd096e3ab06ed3d237417e729908b1211ff
Sushila Burgess <sushila.burgess@bodleian.ox.ac.uk>
2013-12-03 12:09:42 +0000

    Added first version of index by author/title. N.B. This currently only works on letter Z due to Unicode decode errors.
    git-svn-id: https://damssupport.bodleian.ox.ac.uk/svn/mlgb3/trunk@235 a75bfd90-2fc9-4c99-ad46-084cd6553aa8

--- apache/urls.py
+++ apache/urls.py
@@ -18,6 +18,9 @@ urlpatterns = patterns('',
 
     ( r'^feeds/',      include( 'mysite.feeds.urls' ) ),
 
+    ( r'^authortitle/',     include( 'mysite.authortitle.urls' )),
+    ( r'^e/authortitle/',   include( 'mysite.authortitle.urls_e' )),
+
     ( r'^e/media/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT } ),
     ( r'^media/(?P<path>.*)$',   'django.views.static.serve', { 'document_root': settings.MEDIA_ROOT } ),
  )
