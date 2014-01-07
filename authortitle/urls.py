4595c74e57ec79a0758ecdefe3b9e62536fdf74d
Sushila Burgess <sushila.burgess@bodleian.ox.ac.uk>
2014-01-07 13:57:36 +0000

    Added function medieval_catalogues() to display author/title index in medieval catalogue order.
    git-svn-id: https://damssupport.bodleian.ox.ac.uk/svn/mlgb3/trunk@322 a75bfd90-2fc9-4c99-ad46-084cd6553aa8

--- authortitle/urls.py
+++ authortitle/urls.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
     ( r'^$',                           'mysite.authortitle.views.browse' ),
     ( r'^browse/$',                    'mysite.authortitle.views.browse' ),
     ( r'^browse/(?P<letter>\w+)/$',    'mysite.authortitle.views.browse' ),
+    ( r'^medieval_catalogues/$',              'mysite.authortitle.views.medieval_catalogues' ),
+    ( r'^medieval_catalogues/(?P<cat>\w+)/$', 'mysite.authortitle.views.medieval_catalogues' ),
     )
 
--- authortitle/urls_e.py
+++ authortitle/urls_e.py
@@ -5,5 +5,7 @@ urlpatterns = patterns('',
     ( r'^$',                           'mysite.authortitle.views.browse_e' ),
     ( r'^browse/$',                    'mysite.authortitle.views.browse_e' ),
     ( r'^browse/(?P<letter>\w+)/$',    'mysite.authortitle.views.browse_e' ),
+    ( r'^medieval_catalogues/$',              'mysite.authortitle.views.medieval_catalogues_e' ),
+    ( r'^medieval_catalogues/(?P<cat>\w+)/$', 'mysite.authortitle.views.medieval_catalogues_e' ),
     )
 
